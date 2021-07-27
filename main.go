607dacae3be6e5cb825389504c22c626afd0c882
caomao <caomao@caomaodeMBP.lan>
2021-07-27 18:06:20 +0800

    s

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
             c.JSON(200, gin.H{
-                    "message": "pong",
+                    "message": "pong粉色的范德萨范德萨发",
             })
     })
     r.Run(":9000") 
