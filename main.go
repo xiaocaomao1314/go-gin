d117866467ca2f85b79a3374e5291a2cd7cec2c5
caomao <caomao@caomaodeMBP.lan>
2021-07-28 10:19:22 +0800

    992

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
             c.JSON(200, gin.H{
-                    "message": "sfdsfd范德萨范德萨发西方的反对是多少的",
+                    "message": "sfdsfd范德萨范德萨发西方第三方似懂非懂但是范德萨发的反对是多少的",
             })
     })
     r.Run(":9000") 
