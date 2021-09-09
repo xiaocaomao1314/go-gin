4897dd18fcc19ce5d0fefdd96167e800c2e35e3b
xiaocaomao1314 <54468149+xiaocaomao1314@users.noreply.github.com>
2021-09-09 17:04:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 
 func main()  {
 	// 绑定路由
-	http.HandleFunc("/", checkout)
+	http.HandleFunc("/", checkout
+	http.HandleFunc("/wx", getWxSign)
 	// 启动监听=j
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
@@ -51,4 +52,187 @@ func checkout(response http.ResponseWriter, request *http.Request)  {
 	} else {
 		fmt.Println("验证失败")
 	}
+}
+func getWxSign(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(3242)
+	var (
+		noncestr, jsapi_ticket, timestamp, url, signature, signatureStr, access_token string
+		wxAccessToken                                                                 WxAccessToken
+		wxJsApiTicket                                                                 WxJsApiTicket
+		wxSignature                                                                   WxSignature
+		wxSignRtn WxSignRtn
+	)
+ 
+	query := r.URL.Query()
+	url = query["url"][0]
+	
+	noncestr = RandStringBytes(16)
+	timestamp = strconv.FormatInt(time.Now().Unix(), 10)
+ 
+	//获取access_token，如果缓存中有，则直接取出数据使用；否则重新调用微信端接口获取
+	client := &http.Client{}
+	if MemoryCacheVar.Get("access_token") == nil {
+		request, _ := http.NewRequest("GET", AccessTokenHost, nil)
+		response, _ := client.Do(request)
+		defer response.Body.Close()
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = err.Error()
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		err = json.Unmarshal(body, &wxAccessToken)
+		if err != nil {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = err.Error()
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		if wxAccessToken.Errcode == 0 {
+			access_token = wxAccessToken.Access_token
+		} else {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = wxAccessToken.Errmsg
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		MemoryCacheVar.Put("access_token", access_token, time.Duration(wxAccessToken.Expires_in)*time.Second)
+ 
+		//获取 jsapi_ticket
+		requestJs, _ := http.NewRequest("GET", JsAPITicketHost+"?access_token="+access_token+"&type=jsapi", nil)
+		responseJs, _ := client.Do(requestJs)
+		defer responseJs.Body.Close()
+		bodyJs, err := ioutil.ReadAll(responseJs.Body)
+		if err != nil {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = err.Error()
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		err = json.Unmarshal(bodyJs, &wxJsApiTicket)
+		if err != nil {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = err.Error()
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		if wxJsApiTicket.Errcode == 0 {
+			jsapi_ticket = wxJsApiTicket.Ticket
+		} else {
+			wxSignRtn.Code = 1
+			wxSignRtn.Msg = wxJsApiTicket.Errmsg
+			fmt.Fprintln(w,wxSignRtn)
+			return
+		}
+		MemoryCacheVar.Put("jsapi_ticket", jsapi_ticket, time.Duration(wxJsApiTicket.Expires_in)*time.Second)
+	} else {
+		//缓存中存在access_token，直接读取
+		access_token = MemoryCacheVar.Get("access_token").(*Item).Value
+		jsapi_ticket = MemoryCacheVar.Get("jsapi_ticket").(*Item).Value
+	}
+    fmt.Println("access_token:",access_token)
+	fmt.Println("jsapi_ticket:",jsapi_ticket)
+ 
+	// 获取 signature
+	signatureStr = "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url=" + url
+	signature = GetSha1(signatureStr)
+ 
+	wxSignature.Url = url
+	wxSignature.Noncestr = noncestr
+	wxSignature.Timestamp = timestamp
+	wxSignature.Signature = signature
+	wxSignature.AppID = AppID
+ 
+	// 返回前端需要的数据
+	wxSignRtn.Code = 0
+	wxSignRtn.Msg = "success"
+	wxSignRtn.Data = wxSignature
+	fmt.Fprintln(w,wxSignRtn)
+}
+ 
+//生成指定长度的字符串
+func RandStringBytes(n int) string {
+	const letterBytes = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[mathRand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+ 
+//SHA1加密
+func GetSha1(data string) string {
+	t := sha1.New()
+	io.WriteString(t, data)
+	return fmt.Sprintf("%x", t.Sum(nil))
+}
+ 
+type WxAccessToken struct {
+	Access_token string `json:"access_token"`
+	Expires_in   int    `json:"expires_in"`
+	Errcode      int    `json:"errcode"`
+	Errmsg       string `json:"errmsg"`
+}
+type WxJsApiTicket struct {
+	Ticket     string `json:"ticket"`
+	Expires_in int    `json:"expires_in"`
+	Errcode    int    `json:"errcode"`
+	Errmsg     string `json:"errmsg"`
+}
+type WxSignature struct {
+	Noncestr  string `json:"noncestr"`
+	Timestamp string `json:"timestamp"`
+	Url       string `json:"url"`
+	Signature string `json:"signature"`
+	AppID     string `json:"appId"`
+}
+ 
+type WxSignRtn struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data WxSignature `json:"data"`
+}
+ 
+// 数据缓存处理
+type Item struct {
+	Value      string
+	CreateTime time.Time
+	LifeTime   time.Duration
+}
+ 
+type MemoryCache struct {
+	sync.RWMutex
+	Items map[string]*Item
+}
+ 
+func (mc *MemoryCache) Put(key string, value string, lifeTime time.Duration) {
+	mc.Lock()
+	defer mc.Unlock()
+	mc.Items[key] = &Item{
+		LifeTime:   lifeTime,
+		Value:      value,
+		CreateTime: time.Now(),
+	}
+}
+ 
+func (mc *MemoryCache) Get(key string) interface{} {
+	mc.RLock()
+	defer mc.RUnlock()
+	if e, ok := mc.Items[key]; ok {
+		if !e.isExpire() {
+			return e
+		} else {
+			delete(mc.Items, key)
+		}
+	}
+	return nil
+}
+ 
+func (e *Item) isExpire() bool {
+	if e.LifeTime == 0 {
+		return false
+	}
+	//根据创建时间和生命周期判断元素是否失效
+	return time.Now().Sub(e.CreateTime) > e.LifeTime
 }
