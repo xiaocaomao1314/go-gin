a3d7e851293d5341978c12b4fdf7fbc09bb971eb
zhouwei <10316468692qq.com>
2021-10-14 15:21:06 +0800

    ./git.sh

--- main.go
+++ main.go
@@ -1,48 +1,52 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
+
 	wechat "github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/cache"
 	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
 	"github.com/silenceper/wechat/v2/officialaccount/message"
 )
-func main(){
+
+func main() {
 	// 配置好路由
-	http.HandleFunc("/",serveWeachat)
-	fmt.Println("wechat server listener at",":8090")
-	err:=http.ListenAndServe(":8090",nil)
-	if err!=nil{
+	http.HandleFunc("/", serveWeachat)
+	fmt.Println("wechat server listener at", ":8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
 		fmt.Printf("start server error , err=%v", err)
 	}
 }
-// 路由调用的函数
-func serveWeachat(rw http.ResponseWriter,req *http.Request){
 
-	fmt.Fprintln(rw,"我怎么看")
-	wc:=wechat.NewWechat()
+// 路由调用的函数
+func serveWeachat(rw http.ResponseWriter, req *http.Request) {
+ fmt.Println("请求一次我做了什么")
+	// fmt.Fprintln(rw,"我怎么看")
+	wc := wechat.NewWechat()
 	// 本地内存保存access_token
 	memory := cache.NewMemory()
 	cfg := &offConfig.Config{
-		AppID:"wx156ac2299ddaba11",
-		AppSecret:"35693de7cc39c29f162b1c161c2db775",
-		Token:"",
-		Cache:memory,
+		AppID:     "wx156ac2299ddaba11",
+		AppSecret: "35693de7cc39c29f162b1c161c2db775",
+		Token:     "caomao",
+		Cache:     memory,
 	}
-   officialAccount := wc.GetOfficialAccount(cfg)
-     server := officialAccount.GetServer(req,rw)
-		//  设置接收消息的处理方法
-		server.SetMessageHandler(func(msg *message.MixMessage) *message.Reply {
+	officialAccount := wc.GetOfficialAccount(cfg)
+	server := officialAccount.GetServer(req, rw)
+	//  设置接收消息的处理方法
+	server.SetMessageHandler(func(msg *message.MixMessage) *message.Reply {
 
-			//回复消息：演示回复用户发送的消息
-			text := message.NewText(msg.Content)
-			return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+		//回复消息：演示回复用户发送的消息
+		text := message.NewText(msg.Content)
+		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
 	})
-	
-		err := server.Serve()
-		if err!=nil {
-			fmt.Println(err)
-			return
-		}
-		server.Send()
+
+	err := server.Serve()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	server.Send()
 }
